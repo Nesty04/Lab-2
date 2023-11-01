acd8c1d012470ef4d9da1611f9e2159627ebfa62
Nesty04 <barkhotkina04@mail.ru>
2023-11-01 18:28:11 +0300

    task_1

--- lab-2.py
+++ lab-2.py
@@ -1,9 +1,13 @@
+
+
 def books():
     with open('books-en.csv','r') as fh:
+        count = 0
         for string in fh:
-            count = 0
-            my_book = [string.split(';')]
-            print(my_book)
+            my_book = string.split(';')
             if len(my_book[1]) > 30:
                 count += 1
-                print(my_book[1])
+        print (count)
+            
+
+print(books())
